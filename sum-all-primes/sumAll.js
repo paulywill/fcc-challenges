6b7d75a169e8c48a15c73a7e06e9896bae61eeda
Paul Gamble <pgamble@gmail.com>
2018-01-04 21:08:07 -0500

    Passing 1 test; input for 3 not working; logic error in for loop

--- sum-all-primes/sumAll.js
+++ sum-all-primes/sumAll.js
@@ -27,59 +27,46 @@ function sumPrimes(num) {
   Ref: A prime number (or a prime) is a natural number greater than 1 that
   has no positive divisors other than 1 and itself.
 */
-  var boolPrime = false
+
+  console.log("num: " + num);
+  var arrPrimes = [];
+  var result = 0;
 
   //error handling
   if (typeof num !== 'number' || !Number.isInteger(num)) {
+    return;
+  }
+
+  //check num is not a prime
+  if (num < 2) {
+    return 1;
+  }
+
+  if (num === 2) {
     return 0;
   }
 
-  //Check the provided number is not a prime.
-  if (number === 2) {
-    return 0;
-  }
-
-  //Find all the prime Numbers add to an array
-  if (number % 2 === 0) ||
-
-
-
-  //total all the prime numers in array
-
-
-  return num;
-}
-
-
-
-sumPrimes(1);
-
-
-/*
-function isPrime(number) {
-  if (typeof number !== 'number' || !Number.isInteger(number)) {
-    return false;
-  }
-
-  if (number < 2) {
-    return false;
-  }
-
-  if (number === 2) {
-    return true;
-  } else if (number % 2 === 0) {
-    return false;
-  }
-
-  for (var i = 3; i*i <= number; i += 2) {
-    if (number % i === 0) {
-      return false;
+// For testing
+if (num < 900){
+  console.log("num is less than 900 it is " + num);
+  for (var i = 3; i*i <= num; i += 2) {
+    if (num % i !== 0) {
+      arrPrimes.push(num);
+      console.log("array num: " + num);
     }
   }
-  return true;
+  //return true;  <---- You're missing something to account for 3.
 
+  result = arrPrimes.reduce(function (a, b) {
+    return a + b;
+  }, 0);
 }
 
-console.log(isPrime(1));
-console.log(isPrime(15485863));
-/*
+  console.log("arrPrimes: " + arrPrimes);
+  console.log("Result: " + result);
+
+  return result;
+}
+
+console.clear();
+sumPrimes(3);
