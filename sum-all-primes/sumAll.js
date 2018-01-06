734fd452c8573799d6210a219201e36199ec5edd
Paul Gamble <pgamble@gmail.com>
2018-01-05 22:03:47 -0500

    final solution to pass fcc's tests

--- sum-all-primes/sumAll.js
+++ sum-all-primes/sumAll.js
@@ -1,27 +1,3 @@
-
-/*
-Sum All Primes
-
-Sum all the prime numbers up to and including the provided number.
-
-A prime number is defined as a number greater than one and having only two divisors,
-one and itself. For example, 2 is a prime number because it's only divisible by one and two.
-
-The provided number may not be a prime.
-
-Remember to use Read-Search-Ask if you get stuck. Try to pair program. Write your own code.
-
-Here are some helpful links:
-
-    For Loops
-
-    Array.prototype.push()
-
-*/
-
-
-
-
 function sumPrimes(num) {
 /*
   Ref: A prime number (or a prime) is a natural number greater than 1 that
@@ -29,8 +5,7 @@ function sumPrimes(num) {
 */
 
   console.log("num: " + num);
-  var arrPrimes = [];
-  var result = 0;
+  var arrSieve = [], arrPrimes = [], result = 0;
 
   //error handling
   if (typeof num !== 'number' || !Number.isInteger(num)) {
@@ -39,33 +14,35 @@ function sumPrimes(num) {
 
   //check num is not a prime
   if (num < 2) {
-    return 1;
-  }
-
-  if (num === 2) {
-    return 0;
+    return;
   }
 
 // For testing
-if (num < 900){
-  console.log("num is less than 900 it is " + num);
-  for (var i = 3; i*i <= num; i += 2) {
-    if (num % i !== 0) {
-      arrPrimes.push(num);
-      console.log("array num: " + num);
+  for (var i = 2; i <= num; i++) {
+    if (!arrSieve[i]) {
+      arrPrimes.push(i);
+      for (var j = i * 2; j <= num; j += i)
+        arrSieve[j] = true;
     }
   }
-  //return true;  <---- You're missing something to account for 3.
-
-  result = arrPrimes.reduce(function (a, b) {
-    return a + b;
-  }, 0);
-}
 
   console.log("arrPrimes: " + arrPrimes);
-  console.log("Result: " + result);
 
-  return result;
+  // ---------------------------------------------------------------------------------------
+  // Commented out the if statement to pass a test on fcc that was violating it's own rules.
+  // num can only be a non-prime number
+  // ---------------------------------------------------------------------------------------
+  //if (arrPrimes.indexOf(num) < 0 ){
+    result = arrPrimes.reduce(function (a, b) {
+      return a + b;
+    }, 0);
+
+    console.log("result: " + result);
+    return result;
+  //}
+
+
+
 }
 
 console.clear();
