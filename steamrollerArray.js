c91058d64b7f9550909330462deb659db0ae5f84
Paul Gamble <pgamble@gmail.com>
2018-01-11 21:33:06 -0500

    R1D13: Finished streamrollerArray with cut and paste snippet

--- steamrollerArray.js
+++ steamrollerArray.js
@@ -1,24 +1,9 @@
 function steamrollArray(arr) {
-// I'm a steamroller, baby
-  for (i = 0; i < arr.length; i++){  //==> For loop not working for me
-    if (Array.isArray(arr[i]) ){
-      flattened(arr);
-    }
-  }
-
-  console.log("arr: " + arr);
-  return arr;
+  //thank you Stackoverflow
+  //https://stackoverflow.com/questions/10865025/merge-flatten-an-array-of-arrays-in-javascript
+  return arr.reduce(function (flat, toFlatten) {
+    return flat.concat(Array.isArray(toFlatten) ? steamrollArray(toFlatten) : toFlatten);
+  }, []);
 }
 
-function flattened(arr){
-  return arr.reduce(
-      ( acc, cur ) => acc.concat(cur),
-      []
-    );
-
-}
-
-
-
-console.clear();
 steamrollArray([1, [2], [3, [[4]]]]);
