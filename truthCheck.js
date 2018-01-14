403097515f816629120aba99e8f0b7ff562a0cd0
Paul Gamble <pgamble@gmail.com>
2018-01-13 22:13:21 -0500

    R1D15: completed truthCheck

--- truthCheck.js
+++ truthCheck.js
@@ -1,29 +1,14 @@
 
 function truthCheck(collection, pre) {
   // Is everyone being true?
-
-  var srcKeys = Object.keys(pre);
-
-  console.log("srcKeys.length: " + srcKeys.length);
-  console.log("pre: " + pre);
-
-
-  for(var i = 0; i < srcKeys.length; i++) {
-
-    console.log(collection[srcKeys[i]]);
-    console.log(pre in collection[srcKeys[i]]);
-
-    console.log( collection[i].pre );
-
-    if ( !(pre in collection[srcKeys[i]]) || (collection[srcKeys[i]] !== pre[srcKeys[i]]) ){
+  for (var k in collection) {
+    console.log(  collection[k][pre]);
+    var preValue = collection[k][pre];
+    if ( !(pre in collection[k]) || (!preValue)   ){
       return false;
     }
-
   }
-
   return true;
-
-
 }
 
 console.clear();
